69924d135d5ebfa2e05071ff63dac32c17e0e66e
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2023-02-04 20:16:49 +0100

    Created stream of input file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import fs from "fs";
+import readline from "readline";
+
+const fileStream = fs.createReadStream("./src/input.txt", {
+  encoding: "utf-8",
+});
+
+const fileLines = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity,
+});
