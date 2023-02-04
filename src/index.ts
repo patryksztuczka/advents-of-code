eb342e507f9bff643a05048ecff9bd806670da4e
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2023-02-04 20:52:00 +0100

    Solved exercises from 02/12/2022

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,52 @@ const fileLines = readline.createInterface({
   crlfDelay: Infinity,
 });
 
+// A and X == rock 1
+// B and Y == paper 2
+// C and Z == scisors 3
+
+let myPoints = 0;
+
+// first strategy
+// const countMyPoints = (opponentMove: string, myMove: string) => {
+//   if (opponentMove === "A") {
+//     if (myMove === "X") myPoints += 3 + 1;
+//     if (myMove === "Y") myPoints += 6 + 2;
+//     if (myMove === "Z") myPoints += 0 + 3;
+//   } else if (opponentMove === "B") {
+//     if (myMove === "X") myPoints += 0 + 1;
+//     if (myMove === "Y") myPoints += 3 + 2;
+//     if (myMove === "Z") myPoints += 6 + 3;
+//   } else if (opponentMove === "C") {
+//     if (myMove === "X") myPoints += 6 + 1;
+//     if (myMove === "Y") myPoints += 0 + 2;
+//     if (myMove === "Z") myPoints += 3 + 3;
+//   }
+// };
+
+// X - lose
+// Y - draw
+// Z - win
+
+// second strategy
+const countMyPoints = (opponentMove: string, ending: string) => {
+  if (ending === "X") {
+    if (opponentMove === "A") myPoints += 0 + 3;
+    if (opponentMove === "B") myPoints += 0 + 1;
+    if (opponentMove === "C") myPoints += 0 + 2;
+  } else if (ending === "Y") {
+    if (opponentMove === "A") myPoints += 3 + 1;
+    if (opponentMove === "B") myPoints += 3 + 2;
+    if (opponentMove === "C") myPoints += 3 + 3;
+  } else if (ending === "Z") {
+    if (opponentMove === "A") myPoints += 6 + 2;
+    if (opponentMove === "B") myPoints += 6 + 3;
+    if (opponentMove === "C") myPoints += 6 + 1;
+  }
+};
+
 for await (const line of fileLines) {
-  console.log(line);
+  countMyPoints(line[0], line[2]);
 }
+
+console.log(myPoints);
